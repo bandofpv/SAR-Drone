442109a3e26b5ca254e6aff6169b92ba3fc1dd18
bandofpv <bandofpv@gmail.com>
2020-07-09 13:32:33 -0700

    edit the send_ned fucntion to fix for loop xpos

--- detection/tracking.py
+++ detection/tracking.py
@@ -149,10 +149,12 @@ def send_ned_velocity(velocity_x, velocity_y, velocity_z, duration):
         0, 0, 0,  # x, y, z acceleration (not supported yet, ignored in GCS_Mavlink)
         0, 0)  # yaw, yaw_rate (not supported yet, ignored in GCS_Mavlink)
 
+    vehicle.send_mavlink(msg)
+
     # send command to vehicle on 1 Hz cycle
-    for x in range(0, duration):
-        vehicle.send_mavlink(msg)
-        time.sleep(0.5)
+    # for x in range(0, duration):
+    #     vehicle.send_mavlink(msg)
+    #     time.sleep(1)
 
 flying = 0
 
